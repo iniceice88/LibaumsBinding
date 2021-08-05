8b234f2352d131d22ce178caeea377b3cb07d11f
libin <libinbin9456@126.com>
2021-08-05 14:54:04 +0800

    sample

---
+++ csharp/LibaumsBinding/Sample/MainActivity.cs
@@ -0,0 +1,75 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Com.Github.Mjdev.Libaums;
+using System;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace LibaumsSample
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override async void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var devices = UsbMassStorageDevice.GetMassStorageDevices(this);
+            var sb = new StringBuilder();
+            foreach (var device in devices)
+            {
+                var name = device.UsbDevice.DeviceName;
+                if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+                {
+                    name = device.UsbDevice.ManufacturerName + " " + device.UsbDevice.ProductName;
+                }
+
+                var permission = await PermissionHelper.RequestUsbPermissionAsync(this, device.UsbDevice);
+                if (permission != PermissionStatus.Granted)
+                {
+                    Console.WriteLine("No permission to access:" + name);
+                    continue;
+                }
+
+                // before interacting with a device you need to call init()!
+                device.Init();
+                // Only uses the first partition on the device
+                var fs = device.Partitions[0].FileSystem;
+                sb.AppendLine($"Name: {name}");
+                sb.AppendLine($"Capacity: {FormatFileSize(fs.Capacity)}");
+                sb.AppendLine($"Occupied Space: {FormatFileSize(fs.OccupiedSpace)}");
+                sb.AppendLine($"Free Space: {FormatFileSize(fs.FreeSpace)}");
+                sb.AppendLine($"Chunk size: {FormatFileSize(fs.ChunkSize)}");
+                sb.AppendLine("======= root files: ======");
+                var root = fs.RootDirectory;
+                foreach (var file in root.ListFiles())
+                {
+                    sb.Append("\t");
+                    sb.Append(file.IsDirectory ? "folder" : "file").Append(":");
+                    sb.AppendLine(file.Name);
+                }
+
+                sb.AppendLine();
+            }
+
+            Console.WriteLine(sb);
+        }
+
+        private string FormatFileSize(long bytes)
+        {
+            return Android.Text.Format.Formatter.FormatShortFileSize(this, bytes);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ csharp/LibaumsBinding/Sample/PermissionHelper.cs
@@ -0,0 +1,78 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Hardware.Usb;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Context = Android.Content.Context;
+
+namespace LibaumsSample
+{
+    public static class PermissionHelper
+    {
+        private const string ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
+
+        private static ManualResetEventSlim _mres;
+        private static TaskCompletionSource<PermissionStatus> _usbPermissionTcs;
+
+        public static Task<PermissionStatus> RequestUsbPermissionAsync(Context ctx, UsbDevice usbDevice, TimeSpan? timeout = null)
+        {
+            timeout ??= TimeSpan.FromSeconds(10);
+            var usbManager = (UsbManager)ctx.GetSystemService(Context.UsbService);
+            if (usbManager.HasPermission(usbDevice))
+                return Task.FromResult(PermissionStatus.Granted);
+
+            var receiver = new UsbPermissionBroadcastReceiver();
+            var filter = new IntentFilter(ACTION_USB_PERMISSION);
+
+            ctx.RegisterReceiver(receiver, filter);
+
+            _usbPermissionTcs = new TaskCompletionSource<PermissionStatus>();
+
+            var permissionIntent = PendingIntent.GetBroadcast(ctx, 0, new Intent(ACTION_USB_PERMISSION), 0);
+            usbManager.RequestPermission(usbDevice, permissionIntent);
+
+            Task.Factory.StartNew(() =>
+            {
+                _mres = new ManualResetEventSlim(false);
+
+                if (!_mres.Wait(timeout.Value))
+                {
+                    _usbPermissionTcs.SetResult(PermissionStatus.Unknown);
+                }
+
+                _usbPermissionTcs = null;
+                _mres = null;
+            });
+
+            _usbPermissionTcs.Task.ContinueWith(t => { ctx.UnregisterReceiver(receiver); });
+            return _usbPermissionTcs.Task;
+        }
+
+        class UsbPermissionBroadcastReceiver : BroadcastReceiver
+        {
+            public override void OnReceive(Context? context, Intent? intent)
+            {
+                string action = intent.Action;
+                if (ACTION_USB_PERMISSION != action)
+                {
+                    return;
+                }
+
+                //UsbAccessory accessory = (UsbAccessory)intent.GetParcelableExtra(UsbManager.ExtraAccessory);
+
+                if (intent.GetBooleanExtra(UsbManager.ExtraPermissionGranted, false))
+                {
+                    _usbPermissionTcs.SetResult(PermissionStatus.Granted);
+                }
+                else
+                {
+                    _usbPermissionTcs.SetResult(PermissionStatus.Denied);
+                }
+
+                _mres.Set();
+            }
+        }
+    }
+}
